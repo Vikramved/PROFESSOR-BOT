dd3ed803ea422726ba9bc2840908224009b44b90
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-04-18 06:02:10 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -51,7 +51,8 @@ async def save_group(bot, message):
                         await (temp.MELCOW['welcome']).delete()
                     except:
                         pass
-                temp.MELCOW['welcome'] = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}</b>")                                                            
+                temp.MELCOW['welcome'] = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}</b>",
+                                                             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("close", callback_data="close_data")]]))                                                        
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
 async def leave_a_chat(bot, message):
